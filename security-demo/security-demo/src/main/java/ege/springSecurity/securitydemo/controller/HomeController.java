e2b95dd4e6d75979ee534ca889e148b8ad5eed09
Ege Barışan <ege.barisan.01@gmail.com>
2022-10-08 15:34:11 +0300

    formatted

--- security-demo/security-demo/src/main/java/ege/springSecurity/securitydemo/controller/HomeController.java
+++ security-demo/security-demo/src/main/java/ege/springSecurity/securitydemo/controller/HomeController.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping
-    public String home(){
+    public String home() {
         return "Hello Spring World!";
     }
 
     @GetMapping("/user")
-    public String user(){
+    public String user() {
         return "Hello User!";
     }
 
     @GetMapping("/admin")
-    public String admin(){
+    public String admin() {
         return "Hello Admin!";
     }
 
